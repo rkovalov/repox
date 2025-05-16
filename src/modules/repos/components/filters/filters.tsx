0983f3bbc7959ee3c9578703d6faaec80548d0cc
Rudolf Kovalov <rudolf.kovalov@gmail.com>
2025-05-16 23:03:49 +0300

    fix(repos): ensure filters are updated properly when modified

--- src/modules/repos/components/filters/filters.tsx
+++ src/modules/repos/components/filters/filters.tsx
@@ -1,5 +1,8 @@
+import { useEffect, useRef, useState } from 'react';
+
 import { css } from '@/../styled-system/css';
 import { Input } from '@/components';
+
 import { useFilters } from '../../hooks';
 
 export const Filters: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({
@@ -7,17 +10,35 @@ export const Filters: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({
   ...otherProps
 }) => {
   const { filters, onFilterChange } = useFilters();
+  const [localFilters, setLocalFilters] = useState(filters);
+  const lastSubmittedFilters = useRef(filters);
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    onFilterChange({ search: event.target.value });
+  const handleLocalFiltersChange = (newFilters: Partial<typeof filters>) => {
+    setLocalFilters((prev) => {
+      onFilterChange(newFilters);
+      const updatedFilters = { ...prev, ...newFilters };
+      lastSubmittedFilters.current = updatedFilters;
+      return updatedFilters;
+    });
   };
+
+  useEffect(() => {
+    if (
+      JSON.stringify(filters) !== JSON.stringify(lastSubmittedFilters.current)
+    ) {
+      setLocalFilters(filters);
+      lastSubmittedFilters.current = filters;
+    }
+  }, [filters]);
 
   return (
     <div className={css({ width: '30%' })} {...otherProps}>
       <Input
         placeholder="Search..."
-        defaultValue={filters.search}
-        onChange={handleInputChange}
+        value={localFilters.search ?? ''}
+        onChange={(event) =>
+          handleLocalFiltersChange({ search: event.target.value })
+        }
       />
     </div>
   );
